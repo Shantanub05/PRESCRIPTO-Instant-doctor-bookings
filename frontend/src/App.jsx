444dfff986be1f7c28692a9dbd051071b059a3a3
Shantanu-Bokey <shantanu.bokey@roxiler.com>
2025-02-03 10:47:34 +0530

    added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,29 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Doctors from './pages/Doctors';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Appointments from './pages/Appointments';
+import BookAppointment from './pages/BookAppointment';
+
+
 const App = () => {
   return (
-    <div >
-      <h1 className="text-3xl font-bold underline">Hello</h1>
+    <div className="mx-4 sm:mx-[10%]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors/:speciality" element={<Doctors />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/appointments" element={<Appointments />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/book-appointment/:docId" element={<BookAppointment />} />
+      </Routes>
     </div>
   );
 }
---
+++ frontend/src/pages/Appointments.jsx
@@ -0,0 +1,9 @@
+const Appointments = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Appointments
