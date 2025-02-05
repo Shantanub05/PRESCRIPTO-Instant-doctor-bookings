ed2d87709cb839ea4bfb9f667a9d1338f7a12a97
Shantanu-Bokey <shantanu.bokey@roxiler.com>
2025-02-05 15:14:50 +0530

    added TopDoctors component, integrated it into Home page; updated SpecialityMenu to scroll to top on click; modified CSS for scrollbar visibility

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -14,7 +14,7 @@ const SpecialityMenu = () => {
       </p>
       <div className="flex sm:justify-center items-center gap-4 pt-5 w-full overflow-scroll ">
         {specialityData.map((item, index) => (
-          <Link className="flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500" to={`/doctors/${item.speciality}`} key={index}>
+          <Link onClick={()=>scrollTo(0,0)} className="flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500" to={`/doctors/${item.speciality}`} key={index}>
             <img className="w-16 sm:w-24 mb-2" src={item.image} alt={item.speciality} />
             <p>{item.speciality}</p>
           </Link>
