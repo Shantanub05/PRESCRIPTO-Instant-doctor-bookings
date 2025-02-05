f87b0e6d851d84ce044b403d9a1f3cf4747c9fca
Shantanu-Bokey <shantanu.bokey@roxiler.com>
2025-02-05 14:37:29 +0530

    added SpecialityMenu component and integrated it into Home page; updated CSS for scrollbar visibility

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import Header from "../components/Header"
+import SpecialityMenu from "../components/SpecialityMenu"
+
 const Home = () => {
   return (
     <div>
-      Home
+      <Header />
+      <SpecialityMenu/>
     </div>
   )
 }
