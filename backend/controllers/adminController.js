c08d96ce472fcbd4bbd8883336a0bf550a2f773e
Shantanu-Bokey <shantanu.bokey@roxiler.com>
2025-02-10 16:26:46 +0530

    enhance admin functionality: add APIs for fetching doctors, appointments, and dashboard data; implement appointment cancellation; improve error handling and validation

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,82 +2,194 @@ import { v2 as cloudinary } from 'cloudinary';
 import doctorModel from '../models/doctorModal.js';
 import validator from 'validator';
 import bcrypt from 'bcrypt';
-import  jwt  from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
+import appointmentModel from '../models/appointmentModel.js';
+import userModel from '../models/userModel.js';
 
 // API for adding doctor
 const addDoctor = async (req, res) => {
-    try {
-        const { name, email, password, speciality, degree, experience, about, fees, address } = req.body;
-      const imageFile = req.file;
+  try {
+    const {
+      name,
+      email,
+      password,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address,
+    } = req.body;
+    const imageFile = req.file;
 
-      //check for empty data 
-      if (!email || !password || !name || !speciality || !degree || !experience || !about || !fees || !address) {
-        return res.json({ success:false ,message:"All fields are required" });
-      }
+    //check for empty data
+    if (
+      !email ||
+      !password ||
+      !name ||
+      !speciality ||
+      !degree ||
+      !experience ||
+      !about ||
+      !fees ||
+      !address
+    ) {
+      return res.json({ success: false, message: 'All fields are required' });
+    }
 
-      //validate email
-      if (!validator.isEmail(email)) {
-        return res.json({
-          success: false, message:'Invalid email'
-        })
-      }
+    //validate email
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: 'Invalid email',
+      });
+    }
 
-      //validate password
-      if (password.length < 8) {
-        return res.json({
-          success:false ,message:'Password must be at least 8 characters'
-        })
-      }
+    //validate password
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: 'Password must be at least 8 characters',
+      });
+    }
 
-      //hasing password
-      const hashedPassword = await bcrypt.hash(password, 10);
+    // Check if email already exists
+    const existingDoctor = await doctorModel.findOne({ email });
+    if (existingDoctor) {
+      return res.json({
+        success: false,
+        message: 'Doctor with this email already exists',
+      });
+    }
 
-      //upload image to cloudinary
-      const imageUpload = await cloudinary.uploader.upload(imageFile.path, { resource_type: "image" });
-      const imageUrl = imageUpload.secure_url;
+    //hasing password
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-      const doctorData = {
-        name, email, password: hashedPassword, speciality, degree, experience, about, fees, address : JSON.parse(address), image: imageUrl, date: Date.now()
-      }
+    //upload image to cloudinary
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+      resource_type: 'image',
+    });
+    const imageUrl = imageUpload.secure_url;
 
-      const newDoctorData = new doctorModel(doctorData);
-      newDoctorData.save();
+    const doctorData = {
+      name,
+      email,
+      password: hashedPassword,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address: JSON.parse(address),
+      image: imageUrl,
+      date: Date.now(),
+    };
 
-      res.json({success: true, message:'Doctor added successfully'})
-       
-    } catch (error) {
-      console.log(error)
-      res.json({sucess:false,message:error.message})
+    const newDoctorData = new doctorModel(doctorData);
+    await newDoctorData.save();
+
+    res.json({ success: true, message: 'Doctor added successfully' });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
   }
-  
- 
-}
+};
 
 //API for admin login
 
-const loginAdmin = async (req,res) => {
- try {
-  
-   const { email, password } = req.body;
-   
-   if (!email || !password) {
-     res.json({success:false,message:'All fields are required'})
-   }
+const loginAdmin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-  
+    if (!email || !password) {
+      res.json({ success: false, message: 'All fields are required' });
+    }
 
-   if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
-     const token = jwt.sign(email + password, process.env.JWT_SECRET)
-     res.json({sucess:true, message:'Login successful', token})
-   } else {
-     res.json({success:false,message:'Invalid credentials'})
-   }
+    if (
+      email === process.env.ADMIN_EMAIL &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
+      const token = jwt.sign(email + password, process.env.JWT_SECRET);
+      res.json({ success: true, message: 'Login successful', token });
+    } else {
+      res.json({ success: false, message: 'Invalid credentials' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
+// API for fetching all doctors
 
- } catch (error) {
-  console.log(error);
-  res.json({ sucess: false, message: error.message });
- }
+const allDoctors = async (req, res) => {
+  try {
+    const allDoctors = await doctorModel.find({}).select('-password');
+    return res.json({ success: true, allDoctors });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+// API to get all appointments
+const appointmentsAdmin = async (req, res) => {
+  try {
+    const appointments = await appointmentModel.find({})
+    res.json({ success: true, appointments });
+  } catch (error) {
+     return res.json({ success: false, message: error.message });
+  }
 }
 
-export { addDoctor, loginAdmin };
+// Cancel appointments
+const cancelAppointment = async (req, res) => {
+  try {
+    const {  appointmentId } = req.body;
+    const appointmentData = await appointmentModel.findById(appointmentId);
+    console.log(appointmentData);
+   
+    await appointmentModel.findByIdAndUpdate(appointmentId, {
+      cancelled: true,
+    });
+
+    const { docId, slotDate, slotTime } = appointmentData;
+    const docData = await doctorModel.findById(docId);
+
+    let slots_booked = docData.slots_booked;
+    slots_booked[slotDate] = slots_booked[slotDate].filter(
+      (e) => e !== slotTime
+    );
+    await doctorModel.findByIdAndUpdate(docId, { slots_booked });
+    res.json({ success: true, message: 'Appointment Cancelled!' });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+const dashboardData = async (req,res) => {
+  try {
+    const doctors = await doctorModel.find({})
+    const users = await userModel.find({})
+    const appointments = await appointmentModel.find({})
+
+    const dashData = {
+      doctors: doctors.length,
+      users: users.length,
+      appointments: appointments.length,
+      latestAppointments: appointments.reverse().slice(0,5)
+    };
+
+    res.json({success:true,dashData})
+  } catch (error) {
+     res.json({ success: false, message: error.message });
+  }
+}
+
+export {
+  addDoctor,
+  loginAdmin,
+  allDoctors,
+  appointmentsAdmin,
+  cancelAppointment,
+  dashboardData,
+};
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,11 +1,26 @@
+import express from 'express';
+import {
+  addDoctor,
+  loginAdmin,
+  allDoctors,
+  appointmentsAdmin,
+  cancelAppointment,
+  dashboardData,
+} from '../controllers/adminController.js';
+import {
+  changeAvailability,
+ 
+} from '../controllers/doctorController.js';
+import upload from '../middlewares/multer.js';
+import authAdmin from '../middlewares/authAdmin.js';
+const adminRouter = express.Router();
 
-import express from 'express'
-import { addDoctor, loginAdmin } from '../controllers/adminController.js'
-import upload from '../middlewares/multer.js'
-import authAdmin from '../middlewares/authAdmin.js'
-const adminRouter = express.Router()
-
-adminRouter.post('/add-doctor',authAdmin, upload.single('image'), addDoctor)
+adminRouter.post('/add-doctor', authAdmin, upload.single('image'), addDoctor);
 adminRouter.post('/login', loginAdmin);
+adminRouter.post('/all-doctors', authAdmin, allDoctors);
+adminRouter.post('/change-availability', authAdmin, changeAvailability);
+adminRouter.get('/all-appointments', authAdmin, appointmentsAdmin);
+adminRouter.post('/cancel-appointment', authAdmin, cancelAppointment);
+adminRouter.get('/dashboard', authAdmin, dashboardData);
 
-export default adminRouter
+export default adminRouter;
