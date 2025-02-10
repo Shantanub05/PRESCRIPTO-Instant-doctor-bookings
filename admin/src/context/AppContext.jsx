ca05b1cc1d62e3fb0e2e65974bd09c7fc28a6ddb
Shantanu-Bokey <shantanu.bokey@roxiler.com>
2025-02-10 16:28:50 +0530

    add appointment management features; implement appointment retrieval, completion, and cancellation logic; enhance AppContext with utility functions for age calculation and date formatting; create AllAppointment component for displaying appointments; update Login component for admin and doctor authentication

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -4,7 +4,39 @@ import PropTypes from 'prop-types';
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
-  const value = {};
+  const currency = '$';
+  const calAge = (dob) => {
+    const today = new Date();
+    const birthDate = new Date(dob);
+
+    let age = today.getFullYear() - birthDate.getFullYear()
+
+    return age
+  }
+
+    const months = [
+      '',
+      'JAN',
+      'FEB',
+      'MAR',
+      'APR',
+      'MAY',
+      'JUN',
+      'JUL',
+      'AUG',
+      'SEP',
+      'OCT',
+      'NOV',
+      'DEC',
+    ];
+
+    const slotDateFormat = (slotDate) => {
+      const dateArray = slotDate.split('_');
+      return (
+        dateArray[0] + ' ' + months[Number(dateArray[1])] + ' ' + dateArray[2]
+      );
+    };
+  const value = { calAge, slotDateFormat, currency };
 
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -1,10 +1,80 @@
 import { createContext } from 'react';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
 
 export const DoctorContext = createContext();
 
 const DoctorContextProvider = (props) => {
-  const value = {};
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const [dToken, setDToken] = useState(
+    localStorage.getItem('dToken') ? localStorage.getItem('dToken') : ''
+  );
+  const [appointments, setAppointments] = useState([]);
+  
+  
+  const getAppointments = async() => {
+    try {
+      const { data } = await axios.get(
+        backendUrl + '/api/doctor/doctor-appointments',
+        { headers: { dToken } }
+      );
+      if (data.success) {
+        console.log(data.appointments.reverse());
+        setAppointments(data.appointments.reverse())
+      } else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }
+
+  const markComplete = async (appointmentId) => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + '/api/doctor/complete-appointment', { appointmentId }, { headers: { dToken } })
+        if (data.success) {
+          toast.success(data.message)
+          getAppointments();
+        } else {
+          toast.error(data.message)
+        }
+    } catch (error) {
+       toast.error(error.message);
+    }
+  }
+
+  const markCancel = async (appointmentId) => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + '/api/doctor/cancel-appointment',
+        { appointmentId },
+        { headers: { dToken } }
+      );
+      if (data.success) {
+        toast.success(data.message);
+        getAppointments();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const value = {
+    backendUrl,
+    dToken,
+    setDToken,
+    getAppointments,
+    appointments,
+    setAppointments,
+    markComplete,
+    markCancel,
+  };
 
   return (
     <DoctorContext.Provider value={value}>{props.children}</DoctorContext.Provider>
