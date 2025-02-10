80bb662aafbaace23ba290ed7eebb25896347ed5
Shantanu-Bokey <shantanu.bokey@roxiler.com>
2025-02-10 16:27:36 +0530

    add user and doctor routes; configure CORS with specific options for API requests

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,30 @@ import 'dotenv/config';
 import connectDb from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import adminRouter from './routes/adminRoute.js';
+import doctorRouter from './routes/doctorRoute.js';
+import userRouter from './routes/userRoute.js';
 
 const app = express();
 const port = process.env.PORT || 5000;
 connectDb();
 connectCloudinary();
 
+// Configure CORS with specific options
+app.use(
+  cors({
+    origin: '*', // Allow requests from any origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', 'atoken','token',"dToken"],
+  })
+);
+
 app.use(express.json());
-app.use(cors());
 
 // api-endpoints
 app.use('/api/admin', adminRouter);
+app.use('/api/doctor', doctorRouter);
+app.use('/api/user', userRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello !');
