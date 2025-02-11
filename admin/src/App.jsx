81c51b3ad54ff60d1f4f8e7414712ff22626dada
Shantanu-Bokey <shantanu.bokey@roxiler.com>
2025-02-11 13:07:02 +0530

    add conditional routing for admin and doctor based on authentication tokens

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,7 +5,7 @@ import { useContext } from 'react';
 import { AdminContext } from './context/AdminContext';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes,Navigate } from 'react-router-dom';
 import Dashboard from './pages/Admin/Dashboard';
 import AllAppointment from './pages/Admin/AllAppointment';
 import AddDoctor from './pages/Admin/AddDoctor';
@@ -26,15 +26,33 @@ const App = () => {
         <Sidebar />
         <Routes>
           {/* Admin */}
-          <Route path="/" element={<></>} />
-          <Route path="/admin-dashboard" element={<Dashboard />} />
-          <Route path="/all-appointments" element={<AllAppointment />} />
-          <Route path="/add-doctor" element={<AddDoctor />} />
-          <Route path="/doctor-list" element={<DoctorsList />} />
+          {aToken && (
+            <>
+              <Route
+                path="/"
+                element={<Navigate to="/admin-dashboard" replace />}
+              />
+              <Route path="/admin-dashboard" element={<Dashboard />} />
+              <Route path="/all-appointments" element={<AllAppointment />} />
+              <Route path="/add-doctor" element={<AddDoctor />} />
+              <Route path="/doctor-list" element={<DoctorsList />} />
+            </>
+          )}
           {/* Doctor */}
-          <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
-          <Route path="/doctor-appointments" element={<DoctorAppointment />} />
-          <Route path="/doctor-profile" element={<DoctorProfile />} />
+          {dToken && (
+            <>
+              <Route
+                path="/"
+                element={<Navigate to="/doctor-dashboard" replace />}
+              />
+              <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
+              <Route
+                path="/doctor-appointments"
+                element={<DoctorAppointment />}
+              />
+              <Route path="/doctor-profile" element={<DoctorProfile />} />
+            </>
+          )}
         </Routes>
       </div>
     </div>
